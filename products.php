188a57169c13eb27d28c9cf19f42d71dc19a83f4
aatosmedia <tommi.saksa@aatosmedia.fi>
2022-02-09 14:50:45 +0200

    Initial commit

---
+++ products.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company</title>
+    <link rel="stylesheet" href="company-styles.css">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <p class="company-name"><a href="company.html"><img class="logo" src="images/company-logo.png" alt="Company ltd"></a></p>
+        </header>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>  
+                <li><a href="products.php">Products</a></li>  
+                <li><a href="#">Contact</a></li>  
+            </ul>
+        </nav>
+        <article>
+            <h1>Products</h1>
+            <img class="main-image" src="images/products.jpg" alt="Products">
+            <h2>Product 1</h2>
+            <p>Hasellus ut facilisis ex, ultricies suscipit augue. In hac 
+                habitasse platea dictumst. Etiam ut laoreet tortor. Donec ac lorem quis tellus egestas 
+                rhoncus sit amet vel eros.</p>
+            <h2>Product 2</h2>  
+                Nam convallis dolor vitae justo volutpat rutrum. Ut velit 
+                urna, bibendum auctor semper sit amet, iaculis et neque.
+            </p>
+            
+        </article>
+        <footer>
+            <p>&copy; Company, 2022</p>
+        </footer>
+    </div> <!-- container -->
+</body>
+</html>
